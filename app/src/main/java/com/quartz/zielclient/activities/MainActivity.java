1aae1283907f57e68fed528d9c8f73e8412c8676
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-08-13 21:45:46 +1000

    WIP: signup

--- app/src/main/java/com/quartz/zielclient/activities/MainActivity.java
+++ app/src/main/java/com/quartz/zielclient/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.quartz.zielclient;
+package com.quartz.zielclient.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.quartz.zielclient.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/quartz/zielclient/activities/NotSignedInActivity.java
@@ -0,0 +1,4 @@
+package com.quartz.zielclient.activities;
+
+public class NotSignedInActivity {
+}
