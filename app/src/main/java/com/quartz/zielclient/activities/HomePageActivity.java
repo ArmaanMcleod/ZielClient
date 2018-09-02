72be37a723a860b2982fdd5fdbde5dd86a6fa338
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-09-02 15:45:09 +1000

    Cleared HomePageAcitivty

--- app/src/main/java/com/quartz/zielclient/activities/HomePageActivity.java
+++ app/src/main/java/com/quartz/zielclient/activities/HomePageActivity.java
@@ -1,33 +1,15 @@
 package com.quartz.zielclient.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
-import android.widget.TextView;
 
 import com.quartz.zielclient.R;
-import com.quartz.zielclient.controllers.UserController;
-import com.quartz.zielclient.exceptions.UserNotFoundException;
-import com.quartz.zielclient.models.User;
 
 public class HomePageActivity extends Activity {
-
-  private User user;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home_page);
-
-    try {
-      user = UserController.fetchUser(getIntent().getStringExtra("UID"));
-    } catch (UserNotFoundException e) {
-      Intent intent = new Intent(this, MainActivity.class);
-      startActivity(intent);
-      finish();
-    }
-
-    TextView userText = findViewById(R.id.tempUserView);
-    userText.setText(user.toString());
   }
 }
