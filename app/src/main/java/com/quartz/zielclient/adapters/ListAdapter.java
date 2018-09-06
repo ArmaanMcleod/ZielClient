e17f7e16ef51589e892db6f740cf91b649c5fd92
rene <ngweihow.r@gmail.com>
2018-09-06 17:19:42 +1000

    [WIP] Adding Adapter

---
+++ app/src/main/java/com/quartz/zielclient/adapters/ListAdapter.java
@@ -0,0 +1,69 @@
+package com.quartz.zielclient.adapters;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.quartz.zielclient.R;
+import com.quartz.zielclient.models.ListItem;
+
+import java.util.List;
+
+/**
+ * Adapter class for adapting data to integrate them into the ListViews
+ */
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+
+  private List<ListItem> listItems;
+  private Context context;
+
+  // Constructor
+  public ListAdapter(List<ListItem> listItems, Context context) {
+    this.listItems = listItems;
+    this.context = context;
+  }
+
+  // Create ViewGroup whenever ViewHolder gets instantiated
+  @NonNull
+  @Override
+  public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    // Get context of the view from respective XML
+    View view = LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.assisted_list_item, parent, false);
+
+    // Return view
+    return new ViewHolder(view);
+  }
+
+  // Binding the data to the ViewHolders
+  @Override
+  public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return 0;
+  }
+  
+  /**
+   * ViewHolder class made for this ListAdapter
+   */
+  public class ViewHolder extends RecyclerView.ViewHolder {
+
+    // Defining TextViews of the Assisted List Objects
+    private TextView textViewName;
+    private TextView textViewDesc;
+
+    public ViewHolder(@NonNull View itemView) {
+      super(itemView);
+
+      textViewName = (TextView) itemView.findViewById(R.id.assistedListItemName);
+      textViewDesc = (TextView) itemView.findViewById(R.id.assistedListItemDesc);
+    }
+  }
+}
---
+++ app/src/main/java/com/quartz/zielclient/models/ListItem.java
@@ -0,0 +1,33 @@
+package com.quartz.zielclient.models;
+
+/**
+ * ListItem class for representing server data in any RecyclerView in the front-end
+ */
+public class ListItem {
+
+  private String name;
+  private String description;
+
+  // Constructor
+  public ListItem(String name, String description) {
+    this.name = name;
+    this.description = description;
+  }
+
+  // Getters and Setters
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
