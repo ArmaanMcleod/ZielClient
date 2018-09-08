050bb4779f0c6506135dd8130dea59462562a975
OpticGenius <opticgenius@hotmail.com>
2018-09-08 22:49:19 +1000

    Added street view button

--- app/src/main/java/com/quartz/zielclient/activities/common/MapsActivity.java
+++ app/src/main/java/com/quartz/zielclient/activities/common/MapsActivity.java
@@ -7,6 +7,8 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
