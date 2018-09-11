65ecfa7604553977a8bc8ee0d2a2ae3eb1bd84a5
rene <ngweihow.r@gmail.com>
2018-09-11 16:28:03 +1000

    Cleaned up Carer's home page

--- app/src/main/java/com/quartz/zielclient/activities/CarerHomepageActivity.java
+++ app/src/main/java/com/quartz/zielclient/activities/CarerHomepageActivity.java
@@ -1,16 +1,10 @@
 package com.quartz.zielclient.activities;
 
-import android.app.ProgressDialog;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.icu.util.ChineseCalendar;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,8 +13,6 @@ import com.google.firebase.database.ValueEventListener;
 import com.quartz.zielclient.R;
 import com.quartz.zielclient.adapters.ListAdapter;
 import com.quartz.zielclient.models.ListItem;
-
-import java.time.chrono.JapaneseEra;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
