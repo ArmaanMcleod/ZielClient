b90dabd4ec2aaa5d499f6a7a606c64fa3eaeb891
OpticGenius <opticgenius@hotmail.com>
2018-09-19 00:37:39 +1000

    Changed placeholder text with hint

--- app/src/main/java/com/quartz/zielclient/activities/HomePageActivity.java
+++ app/src/main/java/com/quartz/zielclient/activities/HomePageActivity.java
@@ -1,9 +1,13 @@
 package com.quartz.zielclient.activities;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.quartz.zielclient.R;
+
+import java.util.Objects;
 
 public class HomePageActivity extends Activity {
 
@@ -12,5 +16,13 @@ public class HomePageActivity extends Activity {
     setTheme(R.style.HomeTheme);
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home_page);
+
+    // Create autocomplete bar
+    PlaceAutocompleteFragment placeAutoComplete = (PlaceAutocompleteFragment)
+        getFragmentManager().findFragmentById(R.id.editText);
+    Objects.requireNonNull(placeAutoComplete.getView()).setBackgroundColor(Color.WHITE);
+    placeAutoComplete.setHint("Where To");
+
+    // TODO: Add listener here for autocomplete bar and pass to MapsActivity
   }
 }
