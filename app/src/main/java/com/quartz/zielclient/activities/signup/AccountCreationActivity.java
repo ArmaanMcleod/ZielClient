dcba68ddff7b3f162ff39b0d065e987b89d301d1
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-09-19 21:39:11 +1000

    successfully redirecting user after signup

--- app/src/main/java/com/quartz/zielclient/activities/signup/AccountCreationActivity.java
+++ app/src/main/java/com/quartz/zielclient/activities/signup/AccountCreationActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseUser;
 import com.quartz.zielclient.R;
+import com.quartz.zielclient.activities.CarerHomepageActivity;
 import com.quartz.zielclient.activities.SplashScreenActivity;
 import com.quartz.zielclient.activities.common.LaunchPadActivity;
 import com.quartz.zielclient.user.UserController;
@@ -55,8 +56,16 @@ public class AccountCreationActivity extends AppCompatActivity implements OnClic
   }
 
   private void completeAccountCreation(User user) {
-    Intent intent = new Intent(this, LaunchPadActivity.class);
+    Intent intent = new Intent();
     intent.putExtra("user", user.toBundle());
+
+    // Redirect the user to the appropriate home page
+    if (user.isAssisted()) {
+      intent.setClass(this, LaunchPadActivity.class);
+    } else {
+      intent.setClass(this, CarerHomepageActivity.class);
+    }
+
     startActivity(intent);
     finish();
   }
