a9f8945d2e9db4a6cbe4efd6a57a6ba8b7c26f70
rene <ngweihow.r@gmail.com>
2018-09-20 17:06:45 +1000

    Added negative test case

--- app/src/test/java/com/quartz/zielclient/MessageServiceTest.java
+++ app/src/test/java/com/quartz/zielclient/MessageServiceTest.java
@@ -10,13 +10,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class MessageServiceTest {
 
   private Map<String, Map<String, Object>> testMessageData;
+  private Map<String, Map<String, Object>> testMessageData2;
 
   @Before
   public void setup() {
+    // Testing for correct input
     Map<String, Map<String, Object>> test1 = new HashMap<>();
     Map<String, Object> test1Message = new HashMap<>();
     test1Message.put("username", "wei");
@@ -25,11 +28,34 @@ public class MessageServiceTest {
     test1Message.put("messageTime", 12345L);
     test1.put("testMessage", test1Message);
     testMessageData = test1;
+
+    // Testing for incorrect input
+    Map<String, Map<String, Object>> test2 = new HashMap<>();
+    Map<String, Object> test2Message = new HashMap<>();
+    test2Message.put("username", "Tom");
+    test2Message.put("messageType", "TEXT");
+    test2Message.put("messageTime", 124444L);
+    test2Message.put("Text", "I agree");
+    testMessageData2 = test2;
   }
 
+  /**
+   * Test 1 that should pass the Message Services
+   */
   @Test
   public void deserialise_test() {
+    // Test 1
     Map<String, Message> testMessage = MessageService.deserialiseMessages(testMessageData);
     assertEquals("this code sucks", testMessage.get("testMessage").getMessageValue());
+  }
+  
+  /**
+   * Test 2 which tests the wrong message string for the Message Services
+   * TODO Make this test show up
+   */
+  @Test
+  public void deserialise_test2() {
+    Map<String, Message> testMessage = MessageService.deserialiseMessages(testMessageData2);
+    assertNotEquals("I disagree", testMessage.get("testMessage").getMessageValue());
   }
 }
