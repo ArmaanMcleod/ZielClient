bba342fb401c5e45e112f36c8bda96b3128c0a40
OpticGenius <opticgenius@hotmail.com>
2018-09-28 10:51:14 +1000

    Starting test for directionsjsonparser

---
+++ app/src/test/java/com/quartz/zielclient/map/DirectionsJSONParserTest.java
@@ -0,0 +1,23 @@
+package com.quartz.zielclient.map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DirectionsJSONParserTest {
+
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+  }
+
+  @Test
+  public void parse() {
+  }
+}
