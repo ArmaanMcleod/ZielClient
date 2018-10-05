6ae1884b7f5ac3a7aeb61ff86cfed0856e0ab5bb
OpticGenius <opticgenius@hotmail.com>
2018-10-06 09:55:13 +1000

    Added code to test

--- app/src/test/java/com/quartz/zielclient/activities/common/StreetViewActivityTest.java
+++ app/src/test/java/com/quartz/zielclient/activities/common/StreetViewActivityTest.java
@@ -1,5 +1,40 @@
+package com.quartz.zielclient.activities.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Shadows;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowAlertDialog;
+
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.*;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(sdk = 26)
 public class StreetViewActivityTest {
+  StreetViewActivity streetViewActivity = Robolectric.buildActivity(StreetViewActivity.class).create().get();
 
+  // Check that its not null
+  @Test
+  public void testActivityNotNull() {
+    assertNotNull(streetViewActivity);
+  }
+
+  // Tests the dialogue which opens
+  @Test
+  public void testDialogueAppearsOnOpening() {
+    assertNotNull(ShadowAlertDialog.getShownDialogs());
+  }
+
+  // Tests the back close button
+  @Test
+  public void testBackButtonClosesActivity() throws Exception {
+
+    ShadowActivity activityShadow = Shadows.shadowOf(streetViewActivity);
+    streetViewActivity.onBackPressed();
+    assertTrue(activityShadow.isFinishing());
+  }
 }
