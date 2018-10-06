d194db9a3377163a67be29d4d67e59c998d809e2
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-10-06 20:22:15 +1000

    wrote first onboarding activity

--- app/src/main/java/com/quartz/zielclient/activities/common/onboarding/OnBoardingActivity.java
+++ app/src/main/java/com/quartz/zielclient/activities/common/onboarding/OnBoardingActivity.java
@@ -1,11 +1,14 @@
 package com.quartz.zielclient.activities.common.onboarding;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 import com.quartz.zielclient.R;
+import com.quartz.zielclient.activities.signup.SignUpActivity;
 
 public class OnBoardingActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,10 +17,19 @@ public class OnBoardingActivity extends AppCompatActivity implements View.OnClic
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_help_onboarding_page1);
 
+    Button nextButton = findViewById(R.id.next1);
+    nextButton.setOnClickListener(this);
 
+    Button skipButton = findViewById(R.id.signup);
+    skipButton.setOnClickListener(this);
   }
 
   @Override
   public void onClick(View v) {
+    if (v.getId() == R.id.next1) {
+      startActivity(new Intent(this, SecondOnboardingActivity.class));
+    } else if (v.getId() == R.id.signup) {
+      startActivity(new Intent(this, SignUpActivity.class));
+    }
   }
 }
