7c7717cacb2a8627245529ef7d83759f3b14ea19
OpticGenius <opticgenius@hotmail.com>
2018-10-07 15:06:46 +1100

    Created activity

---
+++ app/src/main/java/com/quartz/zielclient/activities/common/TakePhotosActivity.java
@@ -0,0 +1,16 @@
+package com.quartz.zielclient.activities.common;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.quartz.zielclient.R;
+
+public class TakePhotosActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.take_photos);
+  }
+
+}
