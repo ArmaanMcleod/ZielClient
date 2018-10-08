f5f552ee7b5e204e289f472a748756e7532b93cf
OpticGenius <opticgenius@hotmail.com>
2018-10-09 09:58:08 +1100

    Added first version of photo taker

--- app/src/main/java/com/quartz/zielclient/activities/common/TakePhotosActivity.java
+++ app/src/main/java/com/quartz/zielclient/activities/common/TakePhotosActivity.java
@@ -1,8 +1,10 @@
 package com.quartz.zielclient.activities.common;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
 import com.quartz.zielclient.R;
@@ -19,50 +21,64 @@ public class TakePhotosActivity extends AppCompatActivity {
 
   private boolean canTakePicture;
 
+  private final String activity = this.getClass().getSimpleName();
+
+  private final CameraKitEventListener cameraListener = new CameraKitEventListener() {
+    @Override
+    public void onEvent(CameraKitEvent cameraKitEvent) {
+      String s = cameraKitEvent.getType();
+      if (CameraKitEvent.TYPE_CAMERA_OPEN.equals(s)) {
+        Log.d(activity, "Camera enabled");
+        canTakePicture = true;
+
+      } else if (CameraKitEvent.TYPE_CAMERA_CLOSE.equals(s)) {
+        Log.d(activity, "Camera disabled");
+        canTakePicture = false;
+      }
+    }
+
+    @Override
+    public void onError(CameraKitError cameraKitError) {
+      Log.d(activity, "Error opening camera");
+    }
+
+    @Override
+    public void onImage(CameraKitImage cameraKitImage) {
+      Log.d(activity, "Taking picture");
+      byte[] picture = cameraKitImage.getJpeg();
+      Bitmap result = BitmapFactory.decodeByteArray(picture, 0, picture.length);
+    }
+
+    @Override
+    public void onVideo(CameraKitVideo cameraKitVideo) {
+      Log.d(activity, "Taking video");
+    }
+  };
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.take_photos);
 
     cameraView = findViewById(R.id.camera);
+    cameraView.addCameraKitListener(cameraListener);
 
-    cameraView.bindCameraKitListener(new CameraKitEventListener() {
-      @Override
-      public void onEvent(CameraKitEvent cameraKitEvent) {
-        String s = cameraKitEvent.getType();
-        if (CameraKitEvent.TYPE_CAMERA_OPEN.equals(s)) {
-          canTakePicture = true;
-
-        } else if (CameraKitEvent.TYPE_CAMERA_CLOSE.equals(s)) {
-          canTakePicture = false;
-
-        }
-      }
-
-      @Override
-      public void onError(CameraKitError cameraKitError) {
-
-      }
-
-      @Override
-      public void onImage(CameraKitImage cameraKitImage) {
-        Bitmap result = cameraKitImage.getBitmap();
-      }
-
-      @Override
-      public void onVideo(CameraKitVideo cameraKitVideo) {
-
-      }
-    });
+    cameraView.setFlash(CameraKit.Constants.FLASH_ON);
+    cameraView.setFocus(CameraKit.Constants.FOCUS_TAP);
+    cameraView.setMethod(CameraKit.Constants.METHOD_STILL);
+    cameraView.setZoom(CameraKit.Constants.ZOOM_PINCH);
+    cameraView.setPermissions(CameraKit.Constants.PERMISSIONS_STRICT);
+    cameraView.setJpegQuality(100);
 
     Button takePhotoButton = findViewById(R.id.button);
     takePhotoButton.setOnClickListener(v -> {
+      Log.d(activity, "Clicked button");
+
       if (canTakePicture) {
+        Log.d(activity, "Captured image");
         cameraView.captureImage();
       }
     });
-
-
   }
 
   @Override
@@ -76,6 +92,4 @@ public class TakePhotosActivity extends AppCompatActivity {
     cameraView.stop();
     super.onPause();
   }
-
-
 }
