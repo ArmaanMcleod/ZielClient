77e44b501201754eafee09dbd9e5c6571de1b8b2
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-10-15 18:00:40 +1100

    WIP: need to do database updates

--- app/src/main/java/com/quartz/zielclient/activities/common/SettingsActivity.java
+++ app/src/main/java/com/quartz/zielclient/activities/common/SettingsActivity.java
@@ -7,18 +7,25 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 import com.quartz.zielclient.R;
+import com.quartz.zielclient.activities.assisted.AssistedHomePageActivity;
+import com.quartz.zielclient.activities.carer.CarerHomepageActivity;
 import com.quartz.zielclient.user.User;
 import com.quartz.zielclient.user.UserFactory;
 
 public class SettingsActivity extends AppCompatActivity implements ValueEventListener, View.OnClickListener {
 
+  private boolean cancellingChanges = false;
+  private boolean cancelledChanges = false;
   private boolean updating = false;
+
   private User user;
+  private User initialUser;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +34,9 @@ public class SettingsActivity extends AppCompatActivity implements ValueEventLis
     setContentView(R.layout.settings_activity);
 
     Bundle userBundle = getIntent().getBundleExtra("user");
-    user = UserFactory.getUser(userBundle);
+    User thisUser = UserFactory.getUser(userBundle);
+    user = thisUser;
+    initialUser = thisUser;
     populateUi();
 
     ActionBar actionBar = getSupportActionBar();
@@ -63,17 +72,60 @@ public class SettingsActivity extends AppCompatActivity implements ValueEventLis
   public void onClick(View v) {
     switch (v.getId()) {
       case R.id.settingsConfirm:
+        confirmChanges();
+        break;
       case R.id.settingsCancel:
+        cancel();
+        break;
       default:
         break;
     }
   }
 
+  private void confirmChanges() {
+    // todo update database
+  }
+
+  /**
+   * Ignore changes, and go back to the home screen.
+   */
+  private void cancel() {
+    if (updating) {
+      cancelledChanges = true;
+      Toast.makeText(this, "Cancelled changes", Toast.LENGTH_SHORT).show();
+    }
+
+    goHome();
+  }
+
+  /**
+   * Go back to the appropriate home page.
+   */
+  private void goHome() {
+    Class<? extends AppCompatActivity> homePage = user.isAssisted()
+        ? AssistedHomePageActivity.class
+        : CarerHomepageActivity.class;
+    Intent intent = new Intent(this, homePage);
+    startActivity(intent);
+    finish();
+  }
+
   @Override
   public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-    user = UserFactory.getUser(dataSnapshot);
-    updating = false;
-    populateUi();
+    if (cancelledChanges) {
+      // todo set user back to initialuser
+      cancelledChanges = false;
+      cancellingChanges = true;
+      updating = false;
+    } else if (cancellingChanges) {
+      user = UserFactory.getUser(dataSnapshot);
+      cancellingChanges = false;
+      goHome();
+    } else {
+      user = UserFactory.getUser(dataSnapshot);
+      updating = false;
+      populateUi();
+    }
   }
 
   @Override
