21cccae67129682ec469fb3c8a9e8512b9e15dad
Bilal <bshehata@student.unimelb.edu.au>
2018-10-18 18:20:42 +1100

    adjusted comments

--- app/src/main/java/com/quartz/zielclient/activities/carer/CarerMapsActivity.java
+++ app/src/main/java/com/quartz/zielclient/activities/carer/CarerMapsActivity.java
@@ -37,7 +37,6 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -491,8 +490,9 @@ public class CarerMapsActivity extends AppCompatActivity
           startActivity(intent);
           finish();
         });
-
-    endChannelAlertDialog.show();
+    if (!isFinishing()) {
+      endChannelAlertDialog.show();
+      }
   }
 
   /** All messages have been read */
